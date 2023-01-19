81da7fac196b246c5a1e82689e31ac65523e0559
aman7896 <amansagar7896@gmail.com>
2023-01-19 14:25:05 +0530

    Added constructor and GS for Airport model

--- src/main/java/org/testcase/flights/Models/Airport.java
+++ src/main/java/org/testcase/flights/Models/Airport.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Airport {
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
@@ -14,4 +15,52 @@ public class Airport {
     private String location;
     private String code;
     private String zone;
+
+    public Airport(int id, String name, String location, String code, String zone) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.code = code;
+        this.zone = zone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
 }
