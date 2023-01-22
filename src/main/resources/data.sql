ac8ed89141efd25ea3610b0f1d15e281fb5d0c6b
aman7896 <amansagar7896@gmail.com>
2023-01-22 16:04:14 +0530

    Changed id values in insert statements to default

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,20 @@
-INSERT INTO airport (id, code) VALUES ( 1, 'DEL');
-INSERT INTO airport (id, code) VALUES ( 2, 'BLR');
-INSERT INTO airport (id, code) VALUES ( 3, 'MAA');
-INSERT INTO airport (id, code) VALUES ( 4, 'JFK');
-INSERT INTO airport (id, code) VALUES ( 5, 'AMS');
-INSERT INTO airport (id, code) VALUES ( 6, 'DXB');
+INSERT INTO airport (id, code) VALUES ( default, 'DEL');
+INSERT INTO airport (id, code) VALUES ( default, 'BLR');
+INSERT INTO airport (id, code) VALUES ( default, 'MAA');
+INSERT INTO airport (id, code) VALUES ( default, 'JFK');
+INSERT INTO airport (id, code) VALUES ( default, 'AMS');
+INSERT INTO airport (id, code) VALUES ( default, 'DXB');
 
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 1, 'IND0001', 'DEL', 'BLR', 9000000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 2, 'IND0002', 'BLR', 'DEL', 9000000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 3, 'IND0003', 'DEL', 'MAA', 9000000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 4, 'IND0004', 'MAA', 'DEL', 9000000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 5, 'IND0005', 'MAA', 'BLR', 7200000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 6, 'IND0006', 'BLR', 'MAA', 7200000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 7, 'KLM0001', 'DEL', 'JFK', 64800000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 8, 'KLM0002', 'JFK', 'DEL', 64800000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 9, 'KLM0003', 'DEL', 'DXB', 12600000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 10, 'KLM0004', 'DXB', 'DEL', 12600000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 11, 'KLM0005', 'DEL', 'AMS', 27000000);
-INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES ( 12, 'KLM0006', 'AMS', 'DEL', 27000000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'IND0001', 'DEL', 'BLR', 9000000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'IND0002', 'BLR', 'DEL', 9000000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'IND0003', 'DEL', 'MAA', 9000000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'IND0004', 'MAA', 'DEL', 9000000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'IND0005', 'MAA', 'BLR', 7200000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'IND0006', 'BLR', 'MAA', 7200000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'KLM0001', 'DEL', 'JFK', 64800000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'KLM0002', 'JFK', 'DEL', 64800000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'KLM0003', 'DEL', 'DXB', 12600000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'KLM0004', 'DXB', 'DEL', 12600000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'KLM0005', 'DEL', 'AMS', 27000000);
+INSERT INTO flight (id, flightnumber, origin_code, destination_code, duration) VALUES (default, 'KLM0006', 'AMS', 'DEL', 27000000);
 
