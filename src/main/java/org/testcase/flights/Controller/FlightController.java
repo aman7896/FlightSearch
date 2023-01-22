edb583ad80f4e0d4a416b6c2790128a03f53d46a
aman7896 <amansagar7896@gmail.com>
2023-01-22 21:17:34 +0530

    Added implementation for /getall controller

--- src/main/java/org/testcase/flights/Controller/FlightController.java
+++ src/main/java/org/testcase/flights/Controller/FlightController.java
@@ -3,17 +3,62 @@ package org.testcase.flights.Controller;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.testcase.flights.Models.Flight;
 import org.testcase.flights.Repos.FlightRepository;
+import org.testcase.flights.Service.FlightService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/flights")
 public class FlightController {
 
+    @Autowired
+    private FlightService flightService;
+
     @GetMapping("/getall")
-    public JSONArray getAllFlights(){
-        return null;
+    public ResponseEntity<String> getAllFlights(){
+        List<JSONObject> returnFlights = new ArrayList<JSONObject>();
+        List<Flight> flights = flightService.getAllFlights();
+        returnFlights = flights.stream().map(flight -> {
+            JSONObject flightObject = new JSONObject();
+            JSONObject origin = new JSONObject();
+            JSONObject destination = new JSONObject();
+
+            flightObject.put("id",flight.getId());
+            flightObject.put("flightNumber",flight.getFlightNumber());
+
+            origin.put("id",flight.getOrigin().getId());
+            origin.put("code",flight.getOrigin().getCode());
+            flightObject.put("origin", origin);
+
+            destination.put("id",flight.getDestination().getId());
+            destination.put("code",flight.getDestination().getCode());
+            flightObject.put("destination", destination);
+
+            flightObject.put("duration",flight.getDuration());
+
+            JSONArray links = new JSONArray();
+            JSONObject link = new JSONObject();
+
+            links.put(new JSONObject().put("linkRel","linkrels/getbyid/id").put("uri","flights/getbyid/"+flight.getId()))
+                    .put(new JSONObject().put("linkRel","linkrels/update").put("uri","flights/update"))
+                    .put(new JSONObject().put("linkRel","linkrels/getflightbynumber/flightnumber").put("uri","flights/getflightbynumber/"+flight.getFlightNumber()))
+                    .put(new JSONObject().put("linkRel","linkrels/delete").put("uri","flights/delete"))
+                    .put(new JSONObject().put("linkRel","linkrels/getflightbyorigdest/origin/destination")
+                            .put("uri","flights/getflightbyorigdest/"+flight.getOrigin().getCode()+"/"+flight.getDestination().getCode()));
+
+            flightObject.put("links", links);
+
+            return flightObject;
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(returnFlights.toString());
     }
     @GetMapping("/getbyid/{id}")
     public JSONObject getFlightById(@PathVariable int id){
