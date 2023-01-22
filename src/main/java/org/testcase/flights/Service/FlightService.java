492f7e58504a26fdb09b5ed022ed9e1c4e807d78
aman7896 <amansagar7896@gmail.com>
2023-01-22 16:05:22 +0530

    Implemented CRUD methods and requested services

--- src/main/java/org/testcase/flights/Service/FlightService.java
+++ src/main/java/org/testcase/flights/Service/FlightService.java
@@ -1,41 +1,69 @@
 package org.testcase.flights.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.testcase.flights.Models.Airport;
 import org.testcase.flights.Models.Flight;
 import org.testcase.flights.Repos.FlightRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class FlightService {
+
+    Logger logger = LoggerFactory.getLogger(FlightService.class);
 
     @Autowired
     private FlightRepository flightRepository;
 
     public List<Flight> getAllFlights(){
-        return null;
+        return flightRepository.findAll();
     }
 
     public Optional<Flight> getFlightById(int id){
-        return null;
+        return flightRepository.findById(id);
     }
 
     public boolean createFlight(Flight flight){
+        try{
+            flightRepository.save(flight);
+            return true;
+        }
+        catch (Exception e){
+            logger.error("Exception occured while creating flight", e);
+        }
         return false;
     }
 
     public boolean updateFlight(Flight flight){
+        try{
+            flightRepository.save(flight);
+            return true;
+        }
+        catch (Exception e){
+            logger.error("Exception occured while updating flight", e);
+        }
         return false;
     }
 
     public boolean deleteFlight(Flight flight){
+        try{
+            flightRepository.delete(flight);
+            return true;
+        }
+        catch (Exception e){
+            logger.error("Exception occured while deleting flight", e);
+        }
         return false;
     }
 
     public Optional<Flight> getFlightByNumber(String flightnumber){
-        return null;
+        return flightRepository.findByFlightNumber(flightnumber);
     }
 
-    public Optional<Flight> getFlightByOrigDest(String origin, String destination){
-        return null;
+    public List<Flight> getFlightByOrigDest(Airport origin, Airport destination){
+        return flightRepository.findByOriginAndDestination(origin, destination);
     }
 }
