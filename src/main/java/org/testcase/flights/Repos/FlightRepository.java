4097b128f6f8952d49520c156c549646705f978d
aman7896 <amansagar7896@gmail.com>
2023-01-22 16:02:16 +0530

    Added custom JPA methods for getting flight by number or origin-destination combination

--- src/main/java/org/testcase/flights/Repos/FlightRepository.java
+++ src/main/java/org/testcase/flights/Repos/FlightRepository.java
@@ -2,8 +2,14 @@ package org.testcase.flights.Repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.testcase.flights.Models.Airport;
 import org.testcase.flights.Models.Flight;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
+    Optional<Flight> findByFlightNumber(String flightNumber);
+    List<Flight> findByOriginAndDestination(Airport origin, Airport destination);
 }
