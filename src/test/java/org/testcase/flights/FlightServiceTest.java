819007c4f9a7fc204929820ce13bcb52dd3c78e3
aman7896 <amansagar7896@gmail.com>
2023-01-22 16:04:48 +0530

    Changed test implementations to behave properly

--- src/test/java/org/testcase/flights/FlightServiceTest.java
+++ src/test/java/org/testcase/flights/FlightServiceTest.java
@@ -9,6 +9,10 @@ import org.testcase.flights.Models.Airport;
 import org.testcase.flights.Models.Flight;
 import org.testcase.flights.Service.FlightService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootTest
 public class FlightServiceTest {
 
@@ -18,31 +22,35 @@ public class FlightServiceTest {
     @Test
     public void getFlightByIdTest(){
         Flight flight = new Flight(1,"IND0001", new Airport(1,"DEL"), new Airport(2 , "BLR"), 9000000);
-        assertEquals(flight,flightService.getFlightById(1));
+        Optional<Flight> searched = flightService.getFlightById(1);
+        assertEquals(flight.toString(),searched.orElse(null).toString());
     }
     @Test
     public void createFlightTest(){
-        Flight flight = new Flight(13,"KLM0007", new Airport(4,"JFK"), new Airport(5 , "AMS"), 27000000);
+        Flight flight = new Flight( 20,"KLM0007", new Airport(4,"JFK"), new Airport(5 , "AMS"), 27000000);
         assertEquals(true,flightService.createFlight(flight));
     }
     @Test
     public void updateFlightTest(){
         Flight flight = new Flight(1,"IND0999", new Airport(1,"DEL"), new Airport(2 , "BLR"), 9000000);
-        assertEquals(true,flightService.createFlight(flight));
+        assertEquals(true,flightService.updateFlight(flight));
     }
     @Test
     public void deleteFlightTest(){
         Flight flight = new Flight(1,"IND0001", new Airport(1,"DEL"), new Airport(2 , "BLR"), 9000000);
-        assertEquals(flight,flightService.deleteFlight(flight));
+        assertEquals(true,flightService.deleteFlight(flight));
     }
     @Test
     public void getFlightByNumberTest(){
         Flight flight = new Flight(2,"IND0002", new Airport(2,"BLR"), new Airport(1 , "DEL"), 9000000);
-        assertEquals(flight,flightService.getFlightByNumber("IND0002"));
+        Optional<Flight> searched = flightService.getFlightByNumber("IND0002");
+        assertEquals(flight.toString(),searched.orElse(null).toString());
     }
     @Test
     public void getFlightByOrigDestTest(){
         Flight flight = new Flight(2,"IND0002", new Airport(2,"BLR"), new Airport(1 , "DEL"), 9000000);
-        assertEquals(flight,flightService.getFlightByOrigDest("BLR","DEL"));
+        List<Flight> flightList = new ArrayList<Flight>();
+        flightList.add(flight);
+        assertEquals(flightList.toString(),flightService.getFlightByOrigDest(new Airport(2,"BLR"),new Airport(1,"DEL")).toString());
     }
 }
