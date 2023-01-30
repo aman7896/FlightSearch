cd4a676fed7f3e23815b0e48dab0b42cefee2a21
aman7896 <amansagar7896@gmail.com>
2023-01-30 14:14:44 +0530

    Sorted flight responses

--- src/main/java/org/testcase/flights/Controller/FlightController.java
+++ src/main/java/org/testcase/flights/Controller/FlightController.java
@@ -11,7 +11,9 @@ import org.testcase.flights.Models.Flight;
 import org.testcase.flights.Repos.FlightRepository;
 import org.testcase.flights.Service.FlightService;
 
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -27,37 +29,38 @@ public class FlightController {
     public ResponseEntity<String> getAllFlights(){
         List<JSONObject> returnFlights = new ArrayList<JSONObject>();
         List<Flight> flights = flightService.getAllFlights();
-        returnFlights = flights.stream().map(flight -> {
-            JSONObject flightObject = new JSONObject();
-            JSONObject origin = new JSONObject();
-            JSONObject destination = new JSONObject();
+        returnFlights = flights.stream().sorted((Comparator.comparing(Flight::getDuration)))
+            .map(flight -> {
+                JSONObject flightObject = new JSONObject();
+                JSONObject origin = new JSONObject();
+                JSONObject destination = new JSONObject();
 
-            flightObject.put("id",flight.getId());
-            flightObject.put("flightNumber",flight.getFlightNumber());
+                flightObject.put("id",flight.getId());
+                flightObject.put("flightNumber",flight.getFlightNumber());
 
-            origin.put("id",flight.getOrigin().getId());
-            origin.put("code",flight.getOrigin().getCode());
-            flightObject.put("origin", origin);
+                origin.put("id",flight.getOrigin().getId());
+                origin.put("code",flight.getOrigin().getCode());
+                flightObject.put("origin", origin);
 
-            destination.put("id",flight.getDestination().getId());
-            destination.put("code",flight.getDestination().getCode());
-            flightObject.put("destination", destination);
+                destination.put("id",flight.getDestination().getId());
+                destination.put("code",flight.getDestination().getCode());
+                flightObject.put("destination", destination);
 
-            flightObject.put("duration",flight.getDuration());
+                flightObject.put("duration", Duration.ofMillis(flight.getDuration()).toString().substring(2));
 
-            JSONArray links = new JSONArray();
+                JSONArray links = new JSONArray();
 
-            links.put(new JSONObject().put("linkRel","linkrels/getbyid/id").put("uri","flights/getbyid/"+flight.getId()))
-                    .put(new JSONObject().put("linkRel","linkrels/create").put("uri","flights/create"))
-                    .put(new JSONObject().put("linkRel","linkrels/update").put("uri","flights/update"))
-                    .put(new JSONObject().put("linkRel","linkrels/getflightbynumber/flightnumber").put("uri","flights/getflightbynumber/"+flight.getFlightNumber()))
-                    .put(new JSONObject().put("linkRel","linkrels/delete").put("uri","flights/delete"))
-                    .put(new JSONObject().put("linkRel","linkrels/getflightbyorigdest")
-                            .put("uri","flights/getflightbyorigdest"));
+                links.put(new JSONObject().put("linkRel","linkrels/getbyid/id").put("uri","flights/getbyid/"+flight.getId()))
+                        .put(new JSONObject().put("linkRel","linkrels/create").put("uri","flights/create"))
+                        .put(new JSONObject().put("linkRel","linkrels/update").put("uri","flights/update"))
+                        .put(new JSONObject().put("linkRel","linkrels/getflightbynumber/flightnumber").put("uri","flights/getflightbynumber/"+flight.getFlightNumber()))
+                        .put(new JSONObject().put("linkRel","linkrels/delete").put("uri","flights/delete"))
+                        .put(new JSONObject().put("linkRel","linkrels/getflightbyorigdest")
+                                .put("uri","flights/getflightbyorigdest"));
 
-            flightObject.put("links", links);
+                flightObject.put("links", links);
 
-            return flightObject;
+                return flightObject;
         }).collect(Collectors.toList());
 
         return ResponseEntity.status(HttpStatus.OK).body(returnFlights.toString());
@@ -83,7 +86,7 @@ public class FlightController {
             destination.put("code",flight.get().getDestination().getCode());
             flightObject.put("destination", destination);
 
-            flightObject.put("duration",flight.get().getDuration());
+            flightObject.put("duration",Duration.ofMillis(flight.get().getDuration()).toString().substring(2));
 
             links.put(new JSONObject().put("linkRel","linkrels/getall").put("uri","flights/getall"))
                     .put(new JSONObject().put("linkRel","linkrels/getbyid/id").put("uri","flights/getbyid/"+flight.get().getId()))
@@ -200,7 +203,7 @@ public class FlightController {
             destination.put("code",flight.get().getDestination().getCode());
             flightObject.put("destination", destination);
 
-            flightObject.put("duration",flight.get().getDuration());
+            flightObject.put("duration",Duration.ofMillis(flight.get().getDuration()).toString().substring(2));
 
             links.put(new JSONObject().put("linkRel","linkrels/getall").put("uri","flights/getall"))
                     .put(new JSONObject().put("linkRel","linkrels/getbyid/id").put("uri","flights/getbyid/"+flight.get().getId()))
@@ -228,37 +231,38 @@ public class FlightController {
         Airport destination = new Airport((JSONObject) obj.get("destination"));
         List<JSONObject> returnFlights = new ArrayList<JSONObject>();
         List<Flight> flights = flightService.getFlightByOrigDest(origin,destination);
-        returnFlights = flights.stream().map(flight -> {
-            JSONObject flightObject = new JSONObject();
-            JSONObject orig = new JSONObject();
-            JSONObject dest = new JSONObject();
+        returnFlights = flights.stream().sorted((Comparator.comparing(Flight::getDuration)))
+            .map(flight -> {
+                JSONObject flightObject = new JSONObject();
+                JSONObject orig = new JSONObject();
+                JSONObject dest = new JSONObject();
 
-            flightObject.put("id",flight.getId());
-            flightObject.put("flightNumber",flight.getFlightNumber());
+                flightObject.put("id",flight.getId());
+                flightObject.put("flightNumber",flight.getFlightNumber());
 
-            orig.put("id",flight.getOrigin().getId());
-            orig.put("code",flight.getOrigin().getCode());
-            flightObject.put("origin", orig);
+                orig.put("id",flight.getOrigin().getId());
+                orig.put("code",flight.getOrigin().getCode());
+                flightObject.put("origin", orig);
 
-            dest.put("id",flight.getDestination().getId());
-            dest.put("code",flight.getDestination().getCode());
-            flightObject.put("destination", dest);
+                dest.put("id",flight.getDestination().getId());
+                dest.put("code",flight.getDestination().getCode());
+                flightObject.put("destination", dest);
 
-            flightObject.put("duration",flight.getDuration());
+                flightObject.put("duration",Duration.ofMillis(flight.getDuration()).toString().substring(2));
 
-            JSONArray links = new JSONArray();
+                JSONArray links = new JSONArray();
 
-            links.put(new JSONObject().put("linkRel","linkrels/getbyid/id").put("uri","flights/getbyid/"+flight.getId()))
-                    .put(new JSONObject().put("linkRel","linkrels/create").put("uri","flights/create"))
-                    .put(new JSONObject().put("linkRel","linkrels/update").put("uri","flights/update"))
-                    .put(new JSONObject().put("linkRel","linkrels/getflightbynumber/flightnumber").put("uri","flights/getflightbynumber/"+flight.getFlightNumber()))
-                    .put(new JSONObject().put("linkRel","linkrels/delete").put("uri","flights/delete"))
-                    .put(new JSONObject().put("linkRel","linkrels/getflightbyorigdest")
-                            .put("uri","flights/getflightbyorigdest"));
+                links.put(new JSONObject().put("linkRel","linkrels/getbyid/id").put("uri","flights/getbyid/"+flight.getId()))
+                        .put(new JSONObject().put("linkRel","linkrels/create").put("uri","flights/create"))
+                        .put(new JSONObject().put("linkRel","linkrels/update").put("uri","flights/update"))
+                        .put(new JSONObject().put("linkRel","linkrels/getflightbynumber/flightnumber").put("uri","flights/getflightbynumber/"+flight.getFlightNumber()))
+                        .put(new JSONObject().put("linkRel","linkrels/delete").put("uri","flights/delete"))
+                        .put(new JSONObject().put("linkRel","linkrels/getflightbyorigdest")
+                                .put("uri","flights/getflightbyorigdest"));
 
-            flightObject.put("links", links);
+                flightObject.put("links", links);
 
-            return flightObject;
+                return flightObject;
         }).collect(Collectors.toList());
 
         return ResponseEntity.status(HttpStatus.OK).body(returnFlights.toString());
